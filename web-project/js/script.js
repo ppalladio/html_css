7f693d0973a9c906406410f164b4289aaff83f33
ppalladio <nine.slide@gmail.com>
2023-02-12 01:38:10 +0100

    scroll intoview and smooth

--- web-project/js/script.js
+++ web-project/js/script.js
@@ -9,6 +9,31 @@ const headerEl = document.querySelector('.header');
 btnNav.addEventListener('click', (_e) => {
     headerEl.classList.toggle('nav-open');
 });
+
+// scrolling Animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const href = link.getAttribute('href');
+        if (href === '#')
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        if (href !== '#' && href.startsWith('#')) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+        if (link.classList.contains('main-nav-link')) {
+            headerEl.classList.toggle('nav-open');
+        }
+    });
+});
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
     const flex = document.createElement('div');
